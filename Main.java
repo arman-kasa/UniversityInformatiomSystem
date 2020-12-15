12fc289e028dcaf21c620fc81f279838cdd4444e
arman-kasa <54952221+arman-kasa@users.noreply.github.com>
2020-12-15 17:46:57 +0330

    main func

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * make a login form
+         * @auther Arman Kasaiyan
+         */
+        new LogIn_Form();
+    }
+
+}
