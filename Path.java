515de7d1eaf62c249663d399f35002128aa07f5c
arman-kasa <54952221+arman-kasa@users.noreply.github.com>
2020-12-15 17:48:04 +0330

    pathes

---
+++ Path.java
@@ -0,0 +1,15 @@
+public class Path {
+    /**
+     * all paths
+     */
+
+    static String Base = "F:\\term5\\AP\\HW\\Project\\2\\University_Informatiom_System\\";
+
+    static String student       = Base + "src\\Files\\student.txt";
+    static String teacher       = Base + "src\\Files\\teacher.txt";
+    static String Class         = Base + "src\\Files\\Class.txt";
+    static String food          = Base + "src\\Files\\food.txt";
+    static String admin         = Base + "src\\Files\\admin.txt";
+    static String Background    = Base + "src\\Images\\BackGround.jpg";
+
+}
